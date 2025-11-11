a4e549acf996220899b8e203b8dea7e82f3c37e4
Aman Sharma <asharma@integralrsg.com>
2025-11-11 12:43:57 -0800

    refactor(iGSDOF): clean up configuration and remove iGraph references
    - Remove iGraph from TypeScript references in all configs
    
    - Remove unused import from main.tsx
    
    - Clean up commented code
    
    - Simplify component structure

--- iGSDOF/src/main.tsx
+++ iGSDOF/src/main.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
 import { App } from "./App";
-import "@integralrsg/igraph/styles";
-// import { ReportPage } from "./pages/ReportPage";
 
-// const RootComponent = ReportPage;
 const RootComponent =App;
 
 ReactDOM.createRoot(document.getElementById("react-root") as HTMLElement).render(

9287218458b3abb5dcf3efaec6f32a9e069221ac
Aman Sharma <asharma@integralrsg.com>
2025-11-18 14:54:33 -0800

    style: apply consistent formatting to existing files

--- iVentedBlast-js/src/api/api-types.ts
+++ iVentedBlast-js/src/api/api-types.ts
@@ -1,35 +1,34 @@
 export type PostData = {
-    cubicleType: string;
-    fullyVentedType? : string
-    l: number;
-    b: number;
-    h: number;
-    openingFace?: string;
-    openingWidth?: number;
-    openingHeight?: number;
-    chargeWeight: number;
-    chargeStandoff: number;
-    chargeAngle: number;
-    pMax: number;
-    iMax: number;
-}
+  cubicleType: string;
+  fullyVentedType?: string;
+  l: number;
+  b: number;
+  h: number;
+  openingFace?: string;
+  openingWidth?: number;
+  openingHeight?: number;
+  chargeWeight: number;
+  chargeStandoff: number;
+  chargeAngle: number;
+  pMax: number;
+  iMax: number;
+};
 
 export type ResultData = {
-    inputs: PostData;
-    pressure: number;
-} 
-
+  inputs: PostData;
+  pressure: number;
+};
 
 export type Curve = {
-    curve_name: string;
-    xdata: number[];
-    ydata: number[];
-    num_points: number;
+  curve_name: string;
+  xdata: number[];
+  ydata: number[];
+  num_points: number;
 };
 
 export type GrfCurve = {
-    curves: Curve[];
-    filename: string;
-    xlabel: string;
-    ylabel: string;
+  curves: Curve[];
+  filename: string;
+  xlabel: string;
+  ylabel: string;
 };

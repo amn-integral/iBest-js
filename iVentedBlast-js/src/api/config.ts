4aae7b816e5fd4d267994d85a9c50adcdb3e1054
Aman Sharma <asharma@integralrsg.com>
2025-12-01 16:44:47 -0800

    Remove iVentedBlast-js project

--- iVentedBlast-js/src/api/config.ts
+++
@@ -1,2 +0,0 @@
-export const API_URL = '/api/add/';
-export const GRF_URL = 'http://10.71.10.167:8000/api/grf/data/';
--- iVentedBlast-js/src/components/Units.tsx
+++
@@ -1,9 +0,0 @@
-import unitCss from "./Units.module.css";
-
-export const Unit = ({ children }: { children: React.ReactNode }) => (
-  <span className={unitCss.unit}>{children}</span>
-);
-
-export const Sup = ({ children }: { children: React.ReactNode }) => (
-  <sup className={unitCss.sup}>{children}</sup>
-);
